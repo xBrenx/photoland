6aa78e28dcd5a35d551cc7683e167bb90fa69fdb
xBrenx <brenda_panes@hotmail.com>
2023-10-23 19:04:44 -0300

    users modificado

--- api/src/controllers/users.ts
+++ api/src/controllers/users.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { getErrorMessage } from "../lib/utils";
+import { Error, User } from "../lib/datatypes";
 const prisma = new PrismaClient();
 
 export const getAllUsers = async () => {
@@ -11,34 +12,44 @@ export const getAllUsers = async () => {
         }else{
             return AllUsers;
         }
-    } catch (error : unknown) {
-        return {
-            error: getErrorMessage(error),
-        };
+    } catch (error: unknown) {
+        throw new Error(getErrorMessage(error));
     }
 
 };
 
-export const createNewUser = async (email: string, name: string) => {
+export const createNewUser = async (email: string, name: string, userName: string): Promise<User> => {
 
     try{
         
-        if (typeof email === "undefined" || email.trim() === "" || typeof name === "undefined" || name.trim() === ""){
-            return "Missing data!";
+        if (typeof email === "undefined" || email.trim() === "" || typeof name === "undefined" || name.trim() === "" || typeof userName === "undefined" || userName.trim() === ""){
+            throw Error("Missing data!");
         } else  {
             const newUser = await prisma.user.create({
                 data: {
                     email: email,
                     name: name,
+                    userName: userName,
                 }
             });
             return newUser;
          }
         
     } catch ( error: unknown ){
-        return {
-            error: getErrorMessage(error),
-        };
-    }
+        console.log(error)
+     throw Error(getErrorMessage(error));
 
+    }
+};
+
+export const deleteAllUsers = async (id: string) => {
+
+    try {
+        const deleteUser = await prisma.user.delete({
+            where: { id: id},
+        });
+        return deleteUser;
+    } catch ( error: unknown) {
+        throw new Error(getErrorMessage(error));
+    }
 };
