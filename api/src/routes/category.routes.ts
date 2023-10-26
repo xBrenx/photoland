aaf4e9d91cec183125938780498f005607079e2f
xBrenx <brenda_panes@hotmail.com>
2023-10-26 19:49:36 -0300

    ruta category creada

---
+++ api/src/routes/category.routes.ts
@@ -0,0 +1,14 @@
+import { Router, Request, Response } from "express";
+const router = Router();
+
+router.get("/", async ( req: Request, res: Response ) => {
+
+    try {
+        
+    } catch (error: unknown) {
+        
+    }
+
+});
+
+export default router;
