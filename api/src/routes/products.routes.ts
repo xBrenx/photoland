abb1ba893864edcee221623705da96c4b0c495c4
xBrenx <brenda_panes@hotmail.com>
2023-10-26 19:47:51 -0300

    ruta products modificada

--- api/src/routes/products.routes.ts
+++ api/src/routes/products.routes.ts
@@ -1,6 +1,25 @@
 import { Router, Request, Response } from "express";
+import { getAllProducts, getNewProductsFilter } from "../controllers/products";
 const router = Router();
 
 router.get("/", async ( req: Request, res: Response ) => {
+    
+    try {
+        const allProducts = await getAllProducts();
+        res.status(200).send(allProducts);
+    } catch (error: unknown) {
+        res.status(400).send(error);
+    }
+
+});
+
+router.get("/news", async ( req: Request, res: Response ) => {
+
+    try {
+        const newProducts = await getNewProductsFilter();
+        res.status(200).send(newProducts);
+    } catch (error: unknown) {
+        res.status(400).send(error);
+    }
 
 });
